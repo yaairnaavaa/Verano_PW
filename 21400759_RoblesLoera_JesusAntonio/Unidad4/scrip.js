e27ae96ca88658fe1e19716563f0c4247ae47c4e
Jesus <jeanrobleslo@ittepic.edu.mx>
2025-07-21 16:48:27 -0700

    Mapa

--- 21400759_RoblesLoera_JesusAntonio/Unidad4/scrip.js
+++ 21400759_RoblesLoera_JesusAntonio/Unidad4/scrip.js
@@ -36,3 +36,46 @@ const suma = (a,b) => {
 
 suma(5,8);
 
+
+
+// 1.
+/*const num1 = Math.floor(Math.random() * 100) ;
+const num2 = Math.floor(Math.random() * 100) ;
+console.log(num1) 
+console.log(num2);
+if (num1 === num2) {
+    console.log("Los números son iguales:");
+} else {
+    const mayor = num1 > num2 ? num1 : num2;
+    console.log(`El mayor es: ${mayor}`);
+}
+console.log("");
+*/
+// 2. 
+/*function calcularComision(ventas) {
+    if (ventas < 5000 || ventas > 30000) {
+        return "Monto inválido. Debe estar entre $5,000 y $30,000.";
+    }
+    let comision = ventas < 10000 ? ventas * 0.10 : ventas * 0.15;
+    return `Comisión a recibir: $${comision.toFixed(2)}`;
+}
+console.log("Ejercicio 2:");
+let ventas = 12000; 
+console.log(`Ventas: $${ventas}`);
+console.log(calcularComision(ventas));
+console.log("");
+*/
+// 3.
+const Tabla = Math.floor(Math.random() * 9)+1;
+console.log(`Ejercicio 3:\nTabla de multiplicar del ${Tabla}:`);
+for (let i = 1; i <= 10; i++) {
+    console.log(`${Tabla} x ${i} = ${Tabla * i}`);
+}
+console.log("");
+
+const numeros = Math.floor(Math.random() * 6-1);
+console.log(numeros)
+for (let i = 0; i < numeros; i++) {
+    const num = Math.floor(Math.random() * 100);
+    console.log(`Número ${i + 1}: ${num}`);
+}
