1f24ad974a31070cdf6629d7b43bbaf306d26613
jolengar <j0l3ng4r@gmail.com>
2025-07-22 17:58:59 -0700

    EXAU4

--- 21400672_Encarnacion_Garcia_Jose_Luis/Unidad 4/script.js
+++ 21400672_Encarnacion_Garcia_Jose_Luis/Unidad 4/script.js
@@ -1,5 +1,6 @@
 //Declaracion de variables
 
+/*
 var nombre1 = "Jose";
 console.log(nombre1);
 nombre1 = "Jose Mod";
@@ -34,3 +35,138 @@ const suma = (a,b) => {
 }
 
 suma(5,2);
+
+let nombre = prompt("Ingresa tu nombre");
+console.log("Nombre: " + nombre);
+alert("Nombre: " + nombre);
+
+let numero = prompt("Ingresa un numero");
+let resultado = parseInt(numero)+5;
+alert("Resultado: " + resultado);
+
+let numeroFlotante = "2.5";
+console.log(numeroFlotante);
+console.log(parseFloat("5.5"));
+
+function calificacion(){
+    let calificacion = parseInt(
+    prompt("Ingresa tu calificacion (0-100)")
+    );
+
+    console.log("Calificacion: " + calificacion);
+
+    if(calificacion < 70){
+        console.log("Reprobado");
+    }else if(calificacion < 80){
+        console.log("Suficiente");
+    }else if(calificacion < 90){
+        console.log("Notable");
+    }else{
+        console.log("Excelente");
+    }
+}  
+
+
+
+calificacion();
+
+function diaSemana(){
+    let dia = prompt("Ingrese nombre del día").toLowerCase();
+    switch(dia){
+        case "lunes":
+            console.log("Hoy es lunes");
+            break;
+        case "martes":
+            console.log("Hoy es martes");
+        case "miercoles":
+            console.log("Hoy es miercoles");
+        case "jueves":
+            console.log("Hoy es jueves");
+        case "viernes":
+            console.log("Hoy es viernes");
+        case "sabado":
+            console.log("Hoy es sabado");
+        case "domingo":
+            console.log("Hoy es domingo");
+        default:
+            console.log("Dia invalido");
+    }
+}
+
+diaSemana(); 
+
+function tablaMultiplicar(n){
+    for(let i=1; i<=10; i++){
+        console.log('${n} X ${i} = ${n*i}');
+    }
+}
+
+tablaMultiplicar(7); 
+
+function saludar(){
+    let nombres = ["Ana","Luis","Carlos","Marta","Pedro"];
+    console.log(nombres);
+
+    for(let i=0; i < nombres.length; i++){
+        console.log('Hola, ${nombres[i]}');
+    }
+}
+
+function map(){
+    const nombres = ["Ana","Luis","Carlos","Marta","Pedro"];
+    console.log(nombres);
+    const pares = 
+    for(let i=0; i < nombres.length; i++){
+        console.log('Hola, ${nombres[i]}');
+    }
+}
+
+saludar(); 
+
+function filter
+
+function reduce(){
+    const numeros = [1,2,3,4];
+    console.log(numeros);
+    const suma = numeros.reduce((acumulador,n) =>
+        (acumulador+n),0
+    );
+    console.log(numeros);
+}
+
+function forEach(){
+    const nombres = ["Ana", "Luis", "Carlos", "Marta", "Pedro"];
+    console.log(nombres);
+    nombres.forEach(name => console.log("Hola "+name));
+}
+
+function objeto(){
+    let persona = {
+    nombre = "",
+    edad = 21,
+    ciudad = "",
+    imprimirNombre: function() {
+        console.log("Nombre: ", this.nombre);
+    }
+    console.log(persona);
+    persona.carrera = "ISC";
+    console.log(persona);
+    delete persona.ciudad;
+    console.log(persona);
+
+    persona.imprimirNombre();
+
+    }
+}
+
+
+
+function map(){
+    const numeros = [1,2,3,4,5];
+    console.log(numeros);
+    const dobles = numeros.map(n => {
+
+    }
+}
+
+map(); */
