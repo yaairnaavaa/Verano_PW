e92b328c93acedd2568c446c6e94d219e1e4c254
Eric Martínez <ergamartinezto@ittepic.edu.mx>
2025-07-22 23:53:22 -0700

    Ejercicios de U3 y U4

--- 21400723_Martínez_Toribio_Eric_Gabriel/U4/Terea1.js
+++ 21400723_Martínez_Toribio_Eric_Gabriel/U4/Terea1.js
@@ -1,67 +1,81 @@
-function NumerosAleatorios() {
-    let num1 = parseInt(Math.random() * 10);
-    let num2 = parseInt(Math.random() * 10);
-    console.log(num1);
-    console.log(num2);
-    if (num1 == num2) {
-        console.log("Los numeros son iguales");
-    } else if (num1 > num2) {
-        console.log("El numero uno es mayor que el dos");
-    } else if (num2 > num1) {
-        console.log("El numero dos es mayor que el uno");
-    }
+//Comparacion de numeros aleatorios
+function compararNumerosAleatorios() {
+  let num1 = parseInt(Math.random() * 10);
+  let num2 = parseInt(Math.random() * 10);
+  console.log(num1);
+  console.log(num2);
+
+  if (num1 === num2) {
+    console.log("Los números son iguales");
+  } else if (num1 > num2) {
+    console.log("El número uno es mayor que el dos");
+  } else {
+    console.log("El número dos es mayor que el uno");
+  }
 }
 
-NumerosAleatorios();
 
-function comision() {
-    let numero = prompt("Ingresa un numero")
-    let resultado = parseInt(numero);
-    if (numero < 5000) {
-        alert("Numero invalido");
-    } else if (numero > 30000) {
+//Calculo de comision
+function calcularComision() {
+  let entrada = prompt("Ingresa un número");
+  let numero = parseInt(entrada);
 
-        alert("Numero invalido");
-    } else if (numero < 10000) {
-        alert("Usted tendra: " + ((numero / 100) * 10) + "$ de comision");
+  if (isNaN(numero) || numero < 5000 || numero > 30000) {
+    alert("Número inválido");
+  } else if (numero < 10000) {
+    alert("Usted tendrá: " + ((numero / 100) * 10) + "$ de comisión");
+  } else {
+    alert("Usted tendrá: " + ((numero / 100) * 15) + "$ de comisión");
+  }
+}
+
+//Tablas de multiplicar
+function mostrarTablaAleatoria() {
+  let numero = Math.floor(Math.random() * 10) + 2;
+  console.log("Del número: " + numero + "\nSu tabla de multiplicar es:");
+  for (let i = 1; i <= 10; i++) {
+    console.log(numero + " x " + i + " = " + numero * i);
+  }
+}
+
+//clasificacion de numeros
+function clasificarNumeros() {
+  const numeros = [];
+  const pares = [];
+  const impares = [];
+
+  for (let i = 0; i < 10; i++) {
+    let entrada = prompt("Ingresa un número entre 1 y 100");
+    let numero = parseInt(entrada);
+
+    if (isNaN(numero) || numero < 1 || numero > 100) {
+      alert("Número inválido. Por favor ingresa un valor entre 1 y 100.");
+      i--;
     } else {
-        alert("Usted tendra: " + ((numero / 100) * 15) + "$ de comision");
+      numeros.push(numero);
+      if (numero % 2 === 0) {
+        pares.push(numero);
+      } else {
+        impares.push(numero);
+      }
     }
-}
-function tres() {
-    let numero = Math.floor(Math.random() * 10) + 2;
-    console.log("Del numero: " + numero + "\n su tabla de multiplicar es:");
-    console.log(numero * 1 + " " + numero * 2 + " " + numero * 3 + " " + numero * 4 + " " + numero * 5 + " " + numero * 6 + " " + numero * 7 + " " + numero * 8 + " " + numero * 9 + " " + numero * 10);
-}
-function Aleatorios() {
-    const numeros = []
-    const pares = []
-    const impares = []
+  }
 
-    for (let i = 0; i < 10; i++) {
-        let entrada = prompt("Ingresa un numero entre 1 y 100");
-        let numero = parseInt(entrada);
-        if (isNaN(numero) || numero < 1 || numero > 100) {
-            alert("Número inválido. Por favor ingresa un valor entre 1 y 100.");
-            i--;
-        } else {
-            numeros.push(numero);
-            if (numero % 2 === 0) {
-                pares.push(numero);
-            } else {
-                impares.push(numero);
-            }
-        }
-    }
-    console.log("En total se tienen " + pares.length + " numeros pares los cuales son: " + pares);
-    console.log("En total se tienen " + pares.length + " numeros impares los cuales son: " + impares);
-}
-function cinco() {
-  let aleatorio 
-    do {
-        aleatorio = Math.floor(Math.random() * 7); 
-        console.log(aleatorio);
-    } while (aleatorio !== 0);
+  console.log("En total se tienen " + pares.length + " números pares: " + pares);
+  console.log("En total se tienen " + impares.length + " números impares: " + impares);
 }
 
-//Clase del 21/07 
+//generar hata 0
+function generarHastaCero() {
+  let aleatorio;
+  do {
+    aleatorio = Math.floor(Math.random() * 7);
+    console.log(aleatorio);
+  } while (aleatorio !== 0);
+}
+
+compararNumerosAleatorios()
+calcularComision()
+mostrarTablaAleatoria()
+clasificarNumeros()
+generarHastaCero()
