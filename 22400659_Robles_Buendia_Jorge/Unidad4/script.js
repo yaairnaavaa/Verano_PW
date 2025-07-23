93e0fd890817b5b62648a342a6c098fdd9b36644
JorgeRoblesBuendia <joroblesbu@ittepic.edu.mx>
2025-07-22 21:52:10 -0700

    ejercicios

---
+++ 22400659_Robles_Buendia_Jorge/Unidad4/script.js
@@ -0,0 +1,43 @@
+console.log("HOLA MUNDOOO");
+
+var nombre = "jorge";
+console.log(nombre);
+
+var nombre1 ="jorge robles";
+console.log(nombre1)
+
+let nombre2 = "papu"
+console.log(nombre2)
+nombre2= "paput prooo"
+console.log(nombre2)
+
+
+if(true){
+    var mensaje1 = "mensaje1";
+    let mensaje2 = "mensaje 2";
+    console.log(mensaje1)
+    console.log(mensaje2)
+
+}
+
+console.log(mensaje1)
+console.log(mensaje2)
+
+
+
+function saludo1() {
+    console.log("gusto saludarte");
+}
+
+const saludo2 = function() {
+    console.log("saludo 2222"+nombre);
+}
+
+
+const suma = (a,b) =>{
+    let s = a+b;
+    console.log(s)
+    console.log("suma");
+}
+
+console.log(suma());
