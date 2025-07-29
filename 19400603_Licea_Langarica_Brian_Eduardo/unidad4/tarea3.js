476e3c38f864a5483ca53494992d9cbe9023a3dd
ALMIGHTO-GOD <spartan936@outlook.com>
2025-07-28 22:33:28 -0700

    algo

--- 19400603_Licea_Langarica_Brian_Eduardo/unidad4/tarea3.js
+++ 19400603_Licea_Langarica_Brian_Eduardo/unidad4/tarea3.js
@@ -1,9 +1,22 @@
 function ej1() {
-    const base = prompt("inserte base");
-    const exponente = prompt("inserte potencia");
-    for (let i = 0; i < potencia; i++) {
-        const acumulado = acumulado*base
- }
+  const base = Number(prompt("Inserte base"));         
+  const potencia = Number(prompt("Inserte potencia")); 
+  let acumulado = 1; 
 
+  for (let i = 0; i < potencia; i++) {
+    acumulado = acumulado * base;
+  }
+
+  console.log("El resultado es " + acumulado);
+}
+
+
+
+
+function ej2() {
+    for (let i = 0; i < 10; i++) {
+     //   let numero[i] = Math.floor(Math.random()*11);
+        
+    }
 }
 ej1();
