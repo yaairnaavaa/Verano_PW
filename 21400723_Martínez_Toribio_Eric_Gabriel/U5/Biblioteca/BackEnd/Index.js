706d423da0ff22598a9d07d1b3e9b3208011109a
Eric Martínez <ergamartinezto@ittepic.edu.mx>
2025-07-28 17:48:27 -0700

    Trabajos U5

---
+++ 21400723_Martínez_Toribio_Eric_Gabriel/U5/Biblioteca/BackEnd/Index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const { default: mongoose } = require('mongoose');
+const mongose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+mongose.connect('mongodb+srv://root:1234567890@books.8bcr4g7.mongodb.net/books-db?retryWrites=true&w=majority&appName=Books')
+.then(() => console.log("Mongo DB conectado"))
+.catch(error => console.log(error.message));
+
+app.use(express.json());
+app.use(cors());
+
+const BooksSchema = new mongose.Schema({
+   titulo: {type:String},
+   auto:  {type:String},
+   stock: {type: Number}
+
+});
+
+const Book = new mongoose.model('Book',BooksSchema);
+
+app.get('/',(req,res) => {
+    res.json({message:"Binevenido"})
+});
+
+app.get('/getBooks',async (req,res) => {
+    try{
+       const books = await Book.find();//Consulata todos los registros del modelo 
+       return res.status(200).json({
+        message: "Libos ontenidos con exito",
+        libros: books
+       });
+    }catch(error){
+        return res.status(500).json({
+            message : "Error al consultar libros",
+            error: error
+        });
+    }
+});
+
+app.post('/newBook',async (req,res) => {
+    try{
+        const { titulo, autor, stock } = req.body;
+        const newBook = new Book({titulo,autor,stock});
+        await newBook.save();
+        
+        return res.status(200).json({message: "Libro creado con exito"});
+    }catch(error){
+        return res.status(500).json({
+            message : "Error al crear libro",
+            error: error
+        });
+    }
+});
+
+app.put('/updateBook/:bookId',async (req,res) => {
+    try{
+        const bookId = req.params.bookId;
+        const newData = req.body;
+
+        const updateBook = await Book.findByIdAndUpdate(bookId, newData);
+        
+        return res.status(200).json({message: "Libro actulizado con exito"});
+    }catch(error){
+        return res.status(500).json({
+            message : "Error al actullizar libro",
+            error: error
+        });
+    }
+});
+
+app.get('/deleteBook/:bookId',async (req,res) => {
+    try{
+        const bookId = req.params.bookId;
+        await Book.findByIdAndUpdate(bookId);
+        
+        return res.status(200).json({message: "Libro eliminado con exito"});
+    }catch(error){
+        return res.status(500).json({
+            message : "Error al actullizar libro",
+            error: error
+        });
+    }
+});
+
+
+
+app.listen(PORT, () => {
+    console.log("Servidor escuchando en http://localhost:"+PORT);
+});
+
